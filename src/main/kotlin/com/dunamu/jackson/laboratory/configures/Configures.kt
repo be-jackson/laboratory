370d7f87308f7e6f6bbe730bddf9a26a33c925c8
jackson <jackson@dunamu.com>
2022-01-06 16:00:27 +0900

    redis cache manager 설정

--- src/main/kotlin/com/dunamu/jackson/laboratory/configures/Configures.kt
+++ src/main/kotlin/com/dunamu/jackson/laboratory/configures/Configures.kt
@@ -1,23 +1,33 @@
 package com.dunamu.jackson.laboratory.configures
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import net.sf.log4jdbc.Log4jdbcProxyDataSource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.stereotype.Component
+import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -40,8 +50,29 @@ class DataSourcePostProcessor : BeanPostProcessor {
 @EnableJpaRepositories(basePackages = ["com.dunamu.jackson"])
 class DefaultConfiguration {
 
+    @Autowired
+    lateinit var redisConnectionFactory: RedisConnectionFactory
+
     @Bean
-    fun cacheManager(): CacheManager = ConcurrentMapCacheManager()
+    fun cacheConfiguration(): RedisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(5))
+        .disableCachingNullValues()
+        .serializeKeysWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+        )
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                GenericJackson2JsonRedisSerializer(
+                    jacksonObjectMapper().apply {
+                        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+
+                        activateDefaultTyping(polymorphicTypeValidator, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
+                        registerModules(JavaTimeModule())
+                    }
+                )
+            )
+        )
 
     @Bean
     fun jsonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
